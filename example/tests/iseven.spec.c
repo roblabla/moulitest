b185ec38e4ddb2fdce00386aa2ddc19a1c5a5306
yyang42 <yan.yaang@gmail.com>
2015-02-07 21:35:35 +0100

    fw working

--- example/tests/iseven.spec.c
+++ example/tests/iseven.spec.c
@@ -1,4 +1,5 @@
 #include <fw.h>
+#include <myheader.h>
 
 static void failing_test(t_test *test)
 {
@@ -10,9 +11,14 @@ static void success_test(t_test *test)
 	mt_assert(strlen("aaa") == 3);
 }
 
+static void test_iseven(t_test *test)
+{
+	mt_assert(ft_iseven(9) == 0);
+}
 
 void	suite_iseven(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, failing_test);
 	SUITE_ADD_TEST(suite, success_test);
+	SUITE_ADD_TEST(suite, test_iseven);
 }
