5f954f30ff7d591040176d09a31e5375ca3a523c
yyang42 <yan.yaang@gmail.com>
2015-02-08 12:45:37 +0100

    remove brew sha

--- moulitest.rb
+++ moulitest.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Moulitest < Formula
 	homepage "https://www.gnu.org/software/wget/"
 	url "https://github.com/yyang42/moulitest/archive/fw.zip"
-	sha1 "fe8d99a91966dc9fda0dd449b4127a434f383670"
+	sha1 ""
 	version "0.4.4"
 
 	def install
