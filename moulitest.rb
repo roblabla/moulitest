8d99dab72dad23957fb400407dbfa03d40c99be1
yyang42 <yan.yaang@gmail.com>
2015-02-08 12:32:40 +0100

    update moulitest version

--- moulitest.rb
+++ moulitest.rb
@@ -4,7 +4,7 @@ class Moulitest < Formula
 	homepage "https://www.gnu.org/software/wget/"
 	url "https://github.com/yyang42/moulitest/archive/v4.zip"
 	sha1 "fe8d99a91966dc9fda0dd449b4127a434f383670"
-	version "0.4.2"
+	version "0.4.3"
 
 	def install
 		bin.install "src/moulitest"
