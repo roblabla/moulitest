fae1546185dd31f01786364a3768fb14f0da31ea
yyang42 <yan.yaang@gmail.com>
2015-02-08 17:27:14 +0100

    clean up

--- examples/simple_project/ft_strlen.spec.c
+++ examples/simple_project/ft_strlen.spec.c
@@ -4,10 +4,16 @@ size_t	ft_strlen(const char *s); // proto
 
 static void test_ft_strlen(t_test *test)
 {
-	mt_assert(ft_strlen("aaa") == 3);
+	mt_assert(ft_strlen("aaa") == (size_t)3);
+}
+
+static void test_ft_strlen_fail(t_test *test)
+{
+	mt_assert(ft_strlen("aaa") == (size_t)4);
 }
 
 void	suite_ft_strlen(t_suite *suite)
 {
 	SUITE_ADD_TEST(suite, test_ft_strlen);
+	SUITE_ADD_TEST(suite, test_ft_strlen_fail);
 }
