e90b01a61556325120a995f3934f11cbca796d5d
yyang42 <yan.yaang@gmail.com>
2015-02-08 12:28:26 +0100

    fix brew

--- moulitest.rb
+++ moulitest.rb
@@ -3,11 +3,11 @@ require 'formula'
 class Moulitest < Formula
 	homepage "https://www.gnu.org/software/wget/"
 	url "https://github.com/yyang42/moulitest/archive/v4.zip"
-	sha1 ""
+	sha1 "fe8d99a91966dc9fda0dd449b4127a434f383670"
 	version "0.4.2"
 
 	def install
-		bin.install "moulitest"
+		bin.install "src/moulitest"
 		(prefix).install "src"
 	end
 end
