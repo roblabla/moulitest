ad1884c031627e9b5eb5d3d1612d9cc9edc2c817
juschaef <juschaef@student.42.fr>
2015-02-08 14:17:29 +0100

    maj version

--- moulitest.rb
+++ moulitest.rb
@@ -4,7 +4,7 @@ class Moulitest < Formula
 	homepage "https://www.gnu.org/software/wget/"
 	url "https://github.com/yyang42/moulitest/archive/fw.zip"
 	sha1 ""
-	version "0.4.4"
+	version "0.4.5"
 
 	def install
 		bin.install "src/moulitest"
